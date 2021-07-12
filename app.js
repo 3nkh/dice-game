ee8d8bec36d070724155177618c88ecb8644677e
nk <enkhtsetsegbayrmagnai@gmail.com>
2021-07-12 23:56:20 +0800

    roll click darhad shoog soligddog bolgoson

--- app.js
+++ app.js
@@ -6,14 +6,27 @@ var scores = [0, 0];
 //Ээлжийн оноог цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 //Шооны аль талаараа буусныг хадглах хувьсагч хэрэгтэй, 1ээс 6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var diceNumber = Math.floor(Math.random() * 6) + 1;
-// 1 oos 6 hurtel sanamsargui tiig gargana
-
-console.log("Шоо :" + diceNumber);
 //Програм эхлэхэд бэлэн болгох
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("score-1").textContent = "0";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none"; // haragdahgui bolgoj bna
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  diceDom.style.display = "block"; //haragddag bolgo bna
+  // 1 oos 6 hurtel sanamsargui tiig gargana
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.src = "dice-" + diceNumber + ".png";
+  if (activePlayer === 0) {
+    roundScore += diceNumber;
+  }
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  if (activePlayer === 0) {
+    document.getElementById("current-0").textContent = "roundScore";
+  }
+});
