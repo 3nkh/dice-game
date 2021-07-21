4526dd339f27e439597be5ec90cdcd5fef05670d
nk <enkhtsetsegbayrmagnai@gmail.com>
2021-07-21 15:20:13 +0800

    togloom ehlehed oorchlogdoh buh zuiliig oruulav

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 // Тоглогчын ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчыг 0, хоёрдугаар тоглогчыг 1 гэж тэмдэглэе
 var activePlayer = 0;
-
 //Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
-var scores = [0, 0];
+var scores = [];
 //Ээлжийн оноог цуглуулж байгаа оноог хадгалах хувьсагч
-var roundScore = 0;
+var roundScore;
 var diceDom = document.querySelector(".dice");
 newGame();
 
 function newGame() {
   //Шооны аль талаараа буусныг хадглах хувьсагч хэрэгтэй, 1ээс 6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
   //Програм эхлэхэд бэлэн болгох
+
+  document.getElementById("name-" + activePlayer).textContent =
+    "PLAYER " + (activePlayer + 1);
+  document
+    .querySelector(".player-" + activePlayer + "-panel")
+    .classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+  activePlayer = 0;
+  scores = [0, 0];
+  roundScore = 0;
   document.getElementById("score-1").textContent = "0";
   document.getElementById("score-0").textContent = "0";
   document.getElementById("current-0").textContent = "0";
@@ -52,9 +63,7 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
     switchToNextPlayer();
   }
 });
-document.querySelector(".btn-new").addEventListener("click", function () {
-  newGame();
-});
+document.querySelector(".btn-new").addEventListener("click", newGame);
 function switchToNextPlayer() {
   //1 буусан тул тоглогчын ээлжийг энэ хэсэгт сольж өгнө.
   document.getElementById("current-" + activePlayer).textContent = "0";
