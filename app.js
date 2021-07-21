75effacebf23bde8875bac697d30e2972098921a
nk <enkhtsetsegbayrmagnai@gmail.com>
2021-07-21 14:50:15 +0800

    new game tovch bolon hold tovchluul winner zarlah zergiig nemsen

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ var activePlayer = 0;
 var scores = [0, 0];
 //Ээлжийн оноог цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
-//Шооны аль талаараа буусныг хадглах хувьсагч хэрэгтэй, 1ээс 6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-//Програм эхлэхэд бэлэн болгох
-document.getElementById("score-1").textContent = "0";
-document.getElementById("score-0").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
-
 var diceDom = document.querySelector(".dice");
-diceDom.style.display = "none"; // haragdahgui bolgoj bna
+newGame();
+
+function newGame() {
+  //Шооны аль талаараа буусныг хадглах хувьсагч хэрэгтэй, 1ээс 6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+  //Програм эхлэхэд бэлэн болгох
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  diceDom.style.display = "none"; // haragdahgui bolgoj bna
+}
 //shoog shideh event listener
 document.querySelector(".btn-roll").addEventListener("click", function () {
   diceDom.style.display = "block"; //shoonii zurgiig web der haragddag bolgo bna
@@ -28,18 +31,7 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
     roundScore += diceNumber;
     document.getElementById("current-" + activePlayer).textContent = roundScore;
   } else {
-    //1 буусан тул тоглогчын ээлжийг энэ хэсэгт сольж өгнө.
-    document.getElementById("current-" + activePlayer).textContent = "0";
-    roundScore = 0;
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.toggle("active");
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    //active player iig shiljuuleh code
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.toggle("active");
-    diceDom.style.display = "none";
+    switchToNextPlayer();
   }
 });
 
@@ -47,15 +39,34 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
   scores[activePlayer] += roundScore;
   document.getElementById("score-" + activePlayer).textContent =
     scores[activePlayer];
+  //ug toglogchiin hojson esehiig shalgah
+  if (scores[activePlayer] >= 10) {
+    document.getElementById("name-" + activePlayer).textContent = "WINNER";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.toggle("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.toggle("active");
+  } else {
+    switchToNextPlayer();
+  }
+});
+document.querySelector(".btn-new").addEventListener("click", function () {
+  newGame();
+});
+function switchToNextPlayer() {
+  //1 буусан тул тоглогчын ээлжийг энэ хэсэгт сольж өгнө.
   document.getElementById("current-" + activePlayer).textContent = "0";
+  roundScore = 0;
   document
     .querySelector(".player-" + activePlayer + "-panel")
     .classList.toggle("active");
+
   activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
   //active player iig shiljuuleh code
   document
     .querySelector(".player-" + activePlayer + "-panel")
     .classList.toggle("active");
-  roundScore = 0;
   diceDom.style.display = "none";
-});
+}
